55d1b229303e1b7e0e1f3cc5a9731d1f78f63c55
nachorios <riosignacio312@gmail.com>
2019-06-03 11:01:14 -0300

    Creacion de la clase persona

---
+++ src/Persona.java
@@ -0,0 +1,63 @@
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String documento;
+    private String direccion;
+
+    Persona(){
+        nombre = " ";
+        apellido = " ";
+        edad = 0;
+        documento = " ";
+        direccion = " ";
+    }
+
+    Persona(String nombre, String apellido, int edad, String documento, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.documento = documento;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
